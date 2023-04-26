0ee4becaa7ba69ebaf203213b20d2e2e5486ce4f
riyadelberkawy <riyad.elberkawy@gmail.com>
2023-04-26 08:07:20 +0200

    init commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,75 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  act,
+} from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  beforeEach(() => {
+    const mockQuestion = {
+      category: "Entertainment: Film",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        'What is the name of the protagonist in the movie "The Terminator"?',
+      correct_answer: "Kyle Reese",
+      incorrect_answers: ["John Connor", "Sarah Connor", "The Terminator"],
+    };
+    // Mock fetch API to return a fixed question
+    act(() => {
+      global.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          json: () => Promise.resolve({ results: [mockQuestion] }),
+        })
+      );
+    });
+  });
+
+  test("renders loading text when question is not fetched yet", () => {
+    render(<App />);
+    const loadingText = screen.getByText("Loading...");
+    expect(loadingText).toBeInTheDocument();
+  });
+
+  test("renders question text when question is fetched", async () => {
+    render(<App />);
+    await waitFor(() => {
+      const questionElement = screen.getByText(
+        /What is the name of the protagonist/i
+      );
+      expect(questionElement).toBeInTheDocument();
+    });
+  });
+
+  test("displays error message if user submits empty answer", async () => {
+    render(<App />);
+    const submitButton = await screen.findByRole("button", { name: /submit/i });
+    fireEvent.click(submitButton);
+    const errorMessage = await screen.findByText(/Please enter an answer/i);
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  test("displays correct message if user submits correct answer", async () => {
+    render(<App />);
+    const answerInput = await screen.findByLabelText(/enter your answer/i);
+    fireEvent.change(answerInput, { target: { value: "Kyle Reese" } });
+    const submitButton = await screen.findByRole("button", { name: /submit/i });
+    fireEvent.click(submitButton);
+    const resultMessage = await screen.findByText(/Correct!/i);
+    expect(resultMessage).toBeInTheDocument();
+  });
+
+  test("displays incorrect message if user submits incorrect answer", async () => {
+    render(<App />);
+    const answerInput = await screen.findByLabelText(/enter your answer/i);
+    fireEvent.change(answerInput, { target: { value: "John Connor" } });
+    const submitButton = await screen.findByRole("button", { name: /submit/i });
+    fireEvent.click(submitButton);
+    const resultMessage = await screen.findByText(/Incorrect!/i);
+    expect(resultMessage).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export type Question = {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+};
+
+export function App() {
+  const [loading, setLoading] = React.useState(false);
+  const [question, setQuestion] = React.useState<Question | null>(null);
+  const [userAnswer, setUserAnswer] = React.useState("");
+  const [submittedWithoutAnswer, setSubmittedWithoutAnswer] =
+    React.useState(false);
+  const [result, setResult] = React.useState<"Correct!" | "Incorrect!" | "">(
+    ""
+  );
+
+  React.useEffect(() => {
+    fetchQuestion();
+  }, []);
+
+  const fetchQuestion = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://opentdb.com/api.php?amount=1");
+      const data = await response.json();
+      const formattedData: Question = {
+        ...data.results[0],
+      };
+      setQuestion(formattedData);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleAnswerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserAnswer(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!userAnswer) {
+      setSubmittedWithoutAnswer(true);
+      return;
+    }
+    if (userAnswer.toLowerCase() === question?.correct_answer.toLowerCase()) {
+      setResult("Correct!");
+    } else {
+      setResult("Incorrect!");
+    }
+    setUserAnswer("");
+    setSubmittedWithoutAnswer(false);
+    fetchQuestion();
+  };
+
+  if (!question) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="title">Trivia Game</h1>
+        <div className="card">
+          <div className="card-header">{question.category}</div>
+          <div className="card-body">
+            <p className="card-text">
+              {loading ? (
+                "Loading..."
+              ) : (
+                <>
+                  <span
+                    dangerouslySetInnerHTML={{ __html: question.question }}
+                  />
+                </>
+              )}
+            </p>
+            <form className="form-group" onSubmit={handleSubmit}>
+              <label htmlFor="answer" hidden>
+                Enter your answer:
+              </label>
+              <input
+                type="text"
+                id="answer"
+                className="form-control"
+                placeholder="Enter your answer"
+                value={userAnswer}
+                onChange={handleAnswerChange}
+                aria-label="Enter your answer"
+              />
+              {submittedWithoutAnswer && (
+                <div className="error" role="alert">
+                  Please enter an answer
+                </div>
+              )}
+              <button
+                className={`btn btn-primary ${loading ? "btn-loading" : ""}`}
+                aria-label="Submit your answer"
+                type="submit"
+              >
+                {loading ? "Loading..." : "Submit"}
+              </button>
+            </form>
+            {result && (
+              <div
+                className={`
+                result result-${result === "Correct!" ? "correct" : "incorrect"}
+              `}
+                role="status"
+                aria-live="assertive"
+              >
+                {result}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
